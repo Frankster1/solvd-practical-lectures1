55dcdd8cfa33b56bdd9fe35558f58473be336322
Frankster1 <franciscocaride99@gmail.com>
2022-07-22 10:54:33 -0300

    First commit

---
+++ src/Sorting.java
@@ -0,0 +1,18 @@
+
+
+class Sorting{
+    public static void main(String[] args){
+        int[] numbers = {3, 7, 6, 13, 33, 9, -100, 25};
+        int aux = 0;
+
+        //Bubble Sorter
+            for (int i = 0; i < (); i++){
+
+                for (int j = 0; j < (); j++){
+                    if (numbers[j] > numbers[j+1])
+                        aux = numbers[j];
+                }
+            }
+        }
+    }
+}
